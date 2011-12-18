a81b1e87c51fcf432442902e25d863193e7346b0
fh_remdul <fh_remdul@f5862252-ea07-0410-9b72-8bb5f969b5b4>
2011-12-18 15:43:20 +0000

    BfMeshview: * fix StaticMesh edges/vertices z-fighting
    git-svn-id: svn://fhdev.dyndns.org/svn/fh2/trunks/fh2/Tools/BfMeshView@17860 f5862252-ea07-0410-9b72-8bb5f969b5b4

--- shaders/staticmesh_vert.glsl
+++ shaders/staticmesh_vert.glsl
@@ -61,5 +61,8 @@ void main()
  }
  
  // vertex position
- gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+ //gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+ vec4 v = gl_ModelViewMatrix * gl_Vertex;
+ v.w -= 0.01;
+ gl_Position = gl_ProjectionMatrix * v;
 }
